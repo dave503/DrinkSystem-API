49d994fe47e4ac398217868e24aa8c18d0402c5b
Ariel Claros <arielopez229422@gmail.com>
2020-02-07 21:10:28 -0600

    Arregalndo cagada del david jajajaja

--- controllers/control.js
+++ controllers/control.js
@@ -1,69 +1,84 @@
-'use strict'
-const Control = require('../model/Control_bebida')
-const User = require('../model/usuarios')
-const Bebida = require('../model/bebidas')
+"use strict";
+const Control = require("../model/Control_bebida");
+const User = require("../model/usuarios");
+const Bebida = require("../model/bebidas");
 
-function Guardar (req, res) {
-    console.log('POST /api/bebidas')
-    console.log(req.body)
-    let control = new Control()
-    
-    control.usuario = req.body.usuario
-    control.bebida = req.body.bebida
-    control.fecha = {
-      dia : req.body.dia,
-      fecha : req.body.fecha,
-      mes : req.body.mes,
-      year : req.body.year,
-      hora : req.body.hora
-    }
-    control.save((err, ok) => {
-      if (err) res.status(500).send({message: `Error al guardar en la base de datos: ${err} `})
-  
-      res.status(200).send({ control: ok })
-    })
-  }
+function Guardar(req, res) {
+  console.log("POST /api/bebidas");
+  console.log(req.body);
+  let control = new Control();
 
-  function Mostrar (req, res) {
-    
-    Control.find({}, (err, ok) => {
-      if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
-      if (!ok) return res.status(404).send({message: 'No existen registros'})
-  
-      res.status(200).send({ ok })
-    })
-  }
+  control.usuario = req.body.usuario;
+  control.bebida = req.body.bebida;
+  control.fecha = {
+    dia: req.body.dia,
+    fecha: req.body.fecha,
+    mes: req.body.mes,
+    year: req.body.year,
+    hora: req.body.hora
+  };
+  control.save((err, ok) => {
+    if (err)
+      res
+        .status(500)
+        .send({ message: `Error al guardar en la base de datos: ${err} ` });
 
-  function GetUser(req, res){
-    User.find({},(err, ok)=>{
-      if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
-      if (!ok) return res.status(404).send({message: 'No existen registros'})
-  
-      res.status(200).send({ ok })
-    })
-  }
+    res.status(200).send({ control: ok });
+  });
+}
 
-  function GetUsuarios (req, res) {
-    let usuarioId = req.params.usuarioId
-  
-    User.findById(usuarioId, (err, usuario) => {
-      if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
-      if (!usuario) return res.status(404).send({message: `El producto no existe`})
-  
-      res.status(200).send({ usuario })
-    })
-  }
+function Mostrar(req, res) {
+  Control.find({}, (err, ok) => {
+    if (err)
+      return res
+        .status(500)
+        .send({ message: `Error al realizar la petición: ${err}` });
+    if (!ok) return res.status(404).send({ message: "No existen registros" });
 
-  function GetBebidas(req, res){
-    Bebida.find({},(err, ok)=>{
-      if (err) return res.status(500).send({message: `Error al realizar la petición: ${err}`})
-      if (!ok) return res.status(404).send({message: 'No existen registros'})
-  
-      res.status(200).send({ ok })
-    })
-  }
+    res.status(200).send({ ok });
+  });
+}
 
-  /*function SetFecha(req, res){
+function GetUser(req, res) {
+  User.find({}, (err, ok) => {
+    if (err)
+      return res
+        .status(500)
+        .send({ message: `Error al realizar la petición: ${err}` });
+    if (!ok) return res.status(404).send({ message: "No existen registros" });
+
+    res.status(200).send({ ok });
+  });
+}
+
+function GetUsuarios(req, res) {
+  let usuarioId = req.params.usuarioId;
+
+  User.findById(usuarioId, (err, usuario) => {
+    if (err)
+      return res
+        .status(500)
+        .send({ message: `Error al realizar la petición: ${err}` });
+    if (!usuario)
+      return res.status(404).send({ message: `El producto no existe` });
+
+    res.status(200).send({ usuario });
+  });
+}
+
+function GetBebidas(req, res) {
+  Bebida.find({}, (err, ok) => {
+    if (err)
+      return res
+        .status(500)
+        .send({ message: `Error al realizar la petición: ${err}` });
+    if (!ok) return res.status(404).send({ message: "No existen registros" });
+
+    res.status(200).send({ ok });
+  });
+}
+
+/*function SetFecha(req, res){
     var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
     var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
     var fecha = new Date()
@@ -71,11 +86,11 @@ function Guardar (req, res) {
       Año: fecha.getFullYear(), Hora: fecha.getHours() + ":" + fecha.getMinutes() + ":" + fecha.getSeconds()})
   }*/
 
-  module.exports={
-      Guardar,
-      Mostrar,
-      GetUsuarios,
-      //SetFecha,
-      GetBebidas,
-      GetUser
-  }
+module.exports = {
+  Guardar,
+  Mostrar,
+  GetUsuarios,
+  //SetFecha,
+  GetBebidas,
+  GetUser
+};
--- model/Control_bebida.js
+++ model/Control_bebida.js
@@ -1,18 +1,17 @@
-
-'use strict'
-const mongoose = require('mongoose')
-const schema = mongoose.Schema
+"use strict";
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
 
 const control = schema({
-    usuario: String,
-    bebida: String,
-    fecha: { 
-        dia: String,
-        fecha: Number,
-        mes: String,
-        year: Number,
-        hora: String
-     },
-})
+  usuario: JSON,
+  bebida: JSON,
+  fecha: {
+    dia: String,
+    fecha: String,
+    mes: String,
+    year: String,
+    hora: String
+  }
+});
 
-module.exports = mongoose.model('control', control)
+module.exports = mongoose.model("control", control);
